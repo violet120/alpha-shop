5c173e9540733c607040197ee9b954d87576f055
violet120 <ai1397de@gmail.com>
2023-02-11 19:18:48 +0800

    將 header 跟 cart 先引入放置。

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Main from "./components/step/main";
+import Header from "./components/header/Header";
+import Cart from "./components/cart/Cart";
+import "./style/main.scss"
 
 function App() {
   return (
     <>
+      <Header />
       <Main />
+      <Cart />
     </>
   );
 }
