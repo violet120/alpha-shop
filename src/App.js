6adce23af9a5f0b4f6de9996e5123c4d6d527278
violet120 <ai1397de@gmail.com>
2023-02-11 15:05:38 +0800

    step統整後，先在 Main component 統一後，傳給 App compoment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import StepProgress from '../src/components/step/StepProgress.js'
-import Step1 from '../src/components/step/Step1.js';
-import ProgressControl from '../src/components/step/ProgressControl.js'
+import Main from "./components/step/main";
 
 function App() {
   return (
     <>
-      <StepProgress />
-      <Step1 />
-      <ProgressControl />
+      <Main />
     </>
   );
 }
