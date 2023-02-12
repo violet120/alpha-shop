cc8c99c5bfd9eb038f6c0b3f8ee67ce1cd8df4f3
violet120 <ai1397de@gmail.com>
2023-02-12 13:20:39 +0800

    modify svg and img

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,3 +1,6 @@
+import { ReactComponent as Minus } from "../../assets/icons/minus.svg"
+import { ReactComponent as Plus } from "../../assets/icons/plus.svg"
+
 export default function Cart() {
   return (
     <section className="cart-container col col-lg-5 col-sm-12">
@@ -5,43 +8,42 @@ export default function Cart() {
 
       <section className="product-list col col-12" data-total-price="0">
         <div className="product-container col col-12" data-count="0" data-price="3999">
-          <img className="img-container" src="./public/images/product-1.jpg" />
+          <img className="img-container" src={require("../../assets/images/product-1.jpg")} alt="" />
           <div className="product-info">
             <div className="product-name">破壞補丁修身牛仔褲</div>
             <div className="product-control-container">
               <div className="product-control">
-                {/* <svg className="product-action minus">
-                  <use xlink:href="#svg-icon-minus"></use>
-                </svg> */}
+                <svg className="product-action minus">
+                  <Minus />
+                </svg>
                 <span className="product-count"></span>
-                {/* <svg className="product-action plus">
-                  <use xlink:href="#svg-icon-plus"></use>
-                </svg> */}
+                <svg className="product-action plus">
+                  <Plus />
+                </svg>
               </div>
             </div>
             <div className="price"></div>
           </div>
         </div>
         <div className="product-container col col-12" data-count="0" data-price="1299">
-          <img className="img-container" src="./public/images/product-2.jpg" />
+          <img className="img-container" src={require("../../assets/images/product-2.jpg")} alt="" />
           <div className="product-info">
             <div className="product-name">刷色直筒牛仔褲</div>
             <div className="product-control-container">
               <div className="product-control">
-                {/* <svg className="product-action minus">
-                  <use xlink:href="#svg-icon-minus"></use>
-                </svg> */}
+                <svg className="product-action minus">
+                  <Minus />
+                </svg>
                 <span className="product-count"></span>
-                {/* <svg className="product-action plus">
-                  <use xlink:href="#svg-icon-plus"></use>
-                </svg> */}
+                <svg className="product-action plus">
+                  <Plus />
+                </svg>
               </div>
             </div>
             <div className="price"></div>
           </div>
         </div>
       </section>
-
       <section className="cart-info shipping col col-12">
         <div className="text">運費</div>
         <div className="price"></div>
