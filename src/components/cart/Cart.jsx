3021d278a9757b8a67e634d6742f1467ca87d48e
violet120 <ai1397de@gmail.com>
2023-02-26 23:51:02 +0800

    Show dynamic amount

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,33 +1,63 @@
 import { ReactComponent as Minus } from "../../assets/icons/minus.svg"
 import { ReactComponent as Plus } from "../../assets/icons/plus.svg"
+import { useState } from "react"
+import imgOne from "../../assets/images/product-1.jpg"
+import imgTwo from "../../assets/images/product-2.jpg"
 
 const cartData = [
   {
     id: '1',
-    name: '貓咪罐罐',
-    img: 'https://picsum.photos/300/300?text=1',
-    price: 100,
-    quantity: 2,
+    name: '破壞補丁修身牛仔褲',
+    img: imgOne,
+    price: 3999,
+    quantity: 1,
   },
   {
     id: '2',
-    name: '貓咪干干',
-    img: 'https://picsum.photos/300/300?text=2',
-    price: 200,
+    name: '刷色直筒牛仔褲',
+    img: imgTwo,
+    price: 1299,
     quantity: 1,
   },
 ]
 
 export default function Cart() {
+  const [count, setCount] = useState(cartData)
+  const totall = 
+  count.map(data => data.price)
+  .reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  })
+  function handleClickButton(id, get) {
+    const updateCartData = count.map((data) => {
+      if (data.id === id) {
+        if (get === "minus" && data.quantity > 1) {
+          return {
+            ...data,
+            quantity: data.quantity - 1,
+          }
+        } else if (get === "plus") {
+          return {
+            ...data,
+            quantity: data.quantity + 1,
+          }
+        }
+      }
+      return data
+    })
+    setCount(updateCartData)
+    
+  }
   return (
     <section className="cart-container col col-lg-5 col-sm-12">
       <h3 class="cart-title">購物籃</h3>
       <section className="product-list col col-12" data-total-price="0">
-        {cartData.map(data => 
+        {count.map(data => 
           <div 
             className="product-container col col-12" 
             data-count="0" 
-            data-price="3999"
+            data-price={data.price}
+            id={data.id}
             key={data.id}
           >
             <img className="img-container" src={data.img} alt="" />
@@ -35,16 +65,12 @@ export default function Cart() {
               <div className="product-name">{data.name}</div>
               <div className="product-control-container">
                 <div className="product-control">
-                  <svg className="product-action minus">
-                    <Minus />
-                  </svg>
+                  <Minus className="product-action minus" onClick={() => handleClickButton(data.id, "minus")} />
                   <span className="product-count">{data.quantity}</span>
-                  <svg className="product-action plus">
-                    <Plus />
-                  </svg>
+                  <Plus className="product-action plus" onClick={() => handleClickButton(data.id, "plus")} />
                 </div>
               </div>
-              <div className="price">${data.price}</div>
+              <div className="price">${data.price * data.quantity}</div>
             </div>
           </div>
           )}
@@ -55,7 +81,7 @@ export default function Cart() {
       </section>
       <section className="cart-info total col col-12">
         <div className="text">小計</div>
-        <div className="price">$300</div>
+        <div className="price">${totall}</div>
       </section>
     </section>
   )
