4909ddfd21207e86499c5b7d8216acd745346540
violet120 <ai1397de@gmail.com>
2023-03-05 21:54:31 +0800

    for changed to htmlFor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
       <div className="header-container mx-auto">
         {/* <!-- navbar-toggle --> */}
         <input id="navbar-toggle" className="navbar-toggle" type="checkbox" />
-        <label for="navbar-toggle" className="burger-container">
+        <label htmlFor="navbar-toggle" className="burger-container">
           <Toggle className="icon-toggle cursor-point" />
         </label>
 
