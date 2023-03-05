9d35de9fca6927a5eb445a281fb9b9cf16981bc2
violet120 <ai1397de@gmail.com>
2023-03-05 21:53:52 +0800

    Modify the shopping cart data rendering method

--- src/components/Step/Step.jsx
+++ src/components/Step/Step.jsx
@@ -5,8 +5,10 @@ import Step3 from "./Step3"
 import ProgressControl from "./ProgressControl"
 import Cart from "../Cart/Cart"
 import { useState } from "react"
+import { CartContext, cartData } from "../Context/CartContext"
 
 export default function Step() {
+  const [currentCart, setCurrentCart] = useState(cartData)
   const [stepNumber, setStepNumber] = useState(1);
   const handleBackButton = (num) => {
     setStepNumber(num - 1)
@@ -17,20 +19,22 @@ export default function Step() {
   return (
     <main className="site-main">
       <div className="main-container">
-        <section className="register-container col col-lg-6 col-sm-12" data-phase={stepNumber} data-total-price="0">
-          <StepProgress />
-          <section className="form-container col col-12">
-            {stepNumber === 1 && <Step1 />}
-            {stepNumber === 2 && <Step2 />}
-            {stepNumber === 3 && <Step3 />}
+        <CartContext.Provider value={[currentCart, setCurrentCart]}>
+          <section className="register-container col col-lg-6 col-sm-12" data-phase={stepNumber} data-total-price="0">
+            <StepProgress />
+            <section className="form-container col col-12">
+              {stepNumber === 1 && <Step1 />}
+              {stepNumber === 2 && <Step2 />}
+              {stepNumber === 3 && <Step3 />}
+            </section>
           </section>
-        </section>
-        <Cart />
-        <ProgressControl 
-          onStepNumber={stepNumber} 
-          onNextButton={handleNextButton}
-          onBackButton={handleBackButton}
-        />
+          <Cart />
+          <ProgressControl
+            onStepNumber={stepNumber}
+            onNextButton={handleNextButton}
+            onBackButton={handleBackButton}
+          />
+        </CartContext.Provider>
       </div>
     </main>
   )
