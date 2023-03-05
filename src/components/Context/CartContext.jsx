fb4421a0c3b7f2e1d9289e65f86fbee0210d3bd5
violet120 <ai1397de@gmail.com>
2023-03-05 21:51:59 +0800

    Context：Cart initialization data

---
+++ src/components/Context/CartContext.jsx
@@ -0,0 +1,22 @@
+import { createContext } from 'react'
+import imgOne from "../../assets/images/product-1.jpg"
+import imgTwo from "../../assets/images/product-2.jpg"
+
+export const cartData = [
+  {
+    id: '1',
+    name: '破壞補丁修身牛仔褲',
+    img: imgOne,
+    price: 3999,
+    quantity: 1,
+  },
+  {
+    id: '2',
+    name: '刷色直筒牛仔褲',
+    img: imgTwo,
+    price: 1299,
+    quantity: 1,
+  },
+]
+
+export const CartContext = createContext(cartData)
